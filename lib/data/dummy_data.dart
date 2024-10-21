117b90d154fb8d39acbce59f1c49ec8869cf797c
Aarushi0010 <mahajanaarushi10@gmail.com>
2024-10-21 19:39:20 +0530

    InkWell

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -5,51 +5,51 @@ const availableCategories = [
   Category(
     id: 'c1',
     title: 'Italian',
-    color: Colors.black,
+    color: Colors.purple,
   ),
   Category(
     id: 'c2',
     title: 'Quick & Easy',
-    color: Colors.black,
+    color: Colors.red,
   ),
   Category(
     id: 'c3',
     title: 'Hamburgers',
-    color: Colors.black,
+    color: Colors.orange,
   ),
   Category(
     id: 'c4',
     title: 'German',
-    color: Colors.black,
+    color: Colors.amber,
   ),
   Category(
     id: 'c5',
     title: 'Light & Lovely',
-    color: Colors.black,
+    color: Colors.blue,
   ),
   Category(
     id: 'c6',
     title: 'Exotic',
-    color: Colors.black,
+    color: Colors.green,
   ),
   Category(
     id: 'c7',
     title: 'Breakfast',
-    color: Colors.black,
+    color: Colors.lightBlue,
   ),
   Category(
     id: 'c8',
     title: 'Asian',
-    color: Colors.black,
+    color: Colors.lightGreen,
   ),
   Category(
     id: 'c9',
     title: 'French',
-    color: Colors.black,
+    color: Colors.pink,
   ),
   Category(
     id: 'c10',
     title: 'Summer',
-    color: Colors.black,
+    color: Colors.teal,
   ),
 ];
