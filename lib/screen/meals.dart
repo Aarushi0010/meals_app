e8f182db76e7c714d9292e7d40ace6c7420d3576
Aarushi0010 <mahajanaarushi10@gmail.com>
2024-10-23 14:50:49 +0530

    added meals content

--- lib/screen/meals.dart
+++ lib/screen/meals.dart
@@ -1,11 +1,55 @@
 import 'package:flutter/material.dart';
+// import 'package:meals_app/data/dummy_data.dart';
+// import 'package:meals_app/main.dart';
+import 'package:meals_app/model/meal.dart';
 
 class MealScreen extends StatelessWidget{
-  const MealScreen({super.key});
+  const MealScreen({super.key, required this.title , required this.meals});
+
+  final String title ;
+  final List<Meal> meals ;
 
   @override
   Widget build(context){
-    return ; 
+    Widget content  =  ListView.builder(
+      itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(meals[index].title));
+
+    if(meals.isEmpty){
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('uh oh.....Nothing to see here',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headlineLarge!
+                    .copyWith(color: Theme
+                    .of(context)
+                    .colorScheme
+                    .onBackground)),
+            const SizedBox(height: 16),
+            Text('Try selecting a different category' ,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(color: Theme
+                    .of(context)
+                    .colorScheme
+                    .onBackground)),
+          ],
+
+        ),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body:content,
+    );
   }
 
 }
