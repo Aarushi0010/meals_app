ba3277a7793bd6976dcdb710cfb729ddb89effc0
Aarushi0010 <mahajanaarushi10@gmail.com>
2024-10-29 23:46:42 +0530

    steps detail

--- lib/screen/meal_detail.dart
+++ lib/screen/meal_detail.dart
@@ -12,10 +12,44 @@ const MealDetailScreen({super.key , required this.meal});
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(meal.imageUrl,
-      fit: BoxFit.cover,
-      width: double.infinity,
-        height:300,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(meal.imageUrl,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height:300,
+            ),
+        
+            const SizedBox(height: 12,),
+            Text('Ingredients' , style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),),
+        
+            const SizedBox(height: 14,),
+            for(final ingredient in meal.ingredients)
+              Text(ingredient,
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+        
+            const SizedBox(height: 20),
+            Text('Steps' , style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),),
+        
+            const SizedBox(height: 14,),
+            for(final step in meal.steps)
+              Text(step,
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
