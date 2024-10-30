643a54a8131fca42525427af7f63b3916ad5ebdd
Aarushi0010 <mahajanaarushi10@gmail.com>
2024-10-30 14:07:50 +0530

    bottom navigation added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals_app/screen/categories.dart';
+// import 'package:meals_app/screen/categories.dart';
 import 'package:meals_app/screen/tabs.dart';
 
 final theme = ThemeData(
